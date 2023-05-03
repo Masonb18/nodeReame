c3f5dfdc600fe2dec17cc0c964a9cf89c3c14867
Masonb18 <bauer.mason01@gmail.com>
2023-05-03 17:11:24 -0500

    Worked on the markdown and made it so it generates the readme

--- index.js
+++ index.js
@@ -1,62 +1,97 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-const {generateMarkdown}= require('./utils/generateMarkdown')
+// const generateMarkdown= require('./utils/generateMarkdown')
 // TODO: Create an array of questions for user input
 const questions = inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'What is your application title',
-            name:'title'
-        },
-        {
-            type: 'input',
-            message: 'Describe your application',
-            name: 'description'
-        },
-        {
-            type: 'input',
-            message: 'What are the installation instructions',
-            name: 'install'
-        },
-        {
-            type: 'input',
-            message:'How do you use the app',
-            name:'usage'
-        },
+.prompt([
+    {
+        type: 'input',
+        message: 'What is your application title',
+        name:'title'
+    },
+    {
+        type: 'input',
+        message: 'Describe your application',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'What are the installation instructions',
+        name: 'install'
+    },
+    {
+        type: 'input',
+        message:'How do you use the app',
+        name:'usage'
+    },
         {
             type: 'list',
             message: 'Choose your license',
-            name:'License',
+            name:'license',
             choices:['Apache license 2.0', 'MIT', 'Boost Software License 1.0']
         },
         {
             type:'input',
             message:'Who contributed to the application',
-            name:'Contributing'
+            name:'contributing'
         },
         {
             type:'input',
             message:'What Tests were ran',
-            name:'Tests'
+            name:'tests'
         },
         {
             type:'input',
             message:'What is your email',
-            name:'Questions'
+            name:'questions'
         }
     ])
     .then((response)=>{
-// This is my data
+        // This is my data
         const readMePage = generateMarkdown(response);
-        
-        fs.writeToFile('readme.md', readMePage, (err) =>
-        err ? console.log(err) : console.log('Successfully created ReadMe.md') 
-        );
+
+        fs.writeFile('readme.md', readMePage, (err) =>{
+            err ? console.log(err) : console.log('Successfully created ReadMe.md') 
+        });
     });
+    
+    
+function generateMarkdown(response) {
+  return `# ${response.title}
 
-
+# Table of Content
+-[description](#description)
+-[installation](#installation)
+-[usage](#usage)
+-[licenses](#licenses)
+-[contribution](#contribution)
+-[test](#test)
+-[email](#email)
+  
+## description:
+    ${response.description}
+  
+## installation:
+    ${response.install}
+  
+## usage:
+    ${response.usage}
+  
+    
+## licenses:
+  
+## contribution:
+    ${response.contributing}
+  
+## test:
+    ${response.tests}
+  
+## email:
+    ${response.email}
+`;
+}
+    
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {
 
